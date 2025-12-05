1cd76ff3b52c748c8a4305692005a400df6e60f0
moffatwarren <moffatwarren@gmail.com>
2025-12-05 00:33:27 -0800

    small change

--- Debian/install-chrome.sh
+++ Debian/install-chrome.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install -y ./google-chrome-stable_current_amd64.deb
+sudo apt install -y google-chrome-stable
