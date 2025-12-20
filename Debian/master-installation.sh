fc408c83c5fdfc9ad7b21286fcd1d3d6dc225678
moffatwarren <moffatwarren@gmail.com>
2025-12-20 09:24:13 -0800

    debian install update

--- Debian/master-installation.sh
+++ Debian/master-installation.sh
@@ -7,15 +7,16 @@ echo "//////////////////////////////////"
 echo ""
 
 . ./install-chrome.sh
-. ./install-kate.sh
+#. ./install-kate.sh
 . ./install-copyq.sh
+. ./install-kwrite.sh
 . ./install-gimp.sh
 . ./install-obs.sh
+. ./install-kdenlive.sh
 #. ./install-parsec.sh
 #. ./install-teamviewer.sh
-. ./install-tailscale.sh
 . ./install-frog.sh
-. ./install-localsend.sh
+#. ./install-localsend.sh
 . ./install-rustdesk.sh
-. ./install-wallpapers.sh
-
+#. ./install-wallpapers.sh
+. ./install-tailscale.sh
